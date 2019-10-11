745ea28726ae5bf1931e8f50fa56d0546675f327
richen <richenlin@gmail.com>
2019-10-11 21:12:06 +0800

    The first available version

--- src/core/Decorators.ts
+++ src/core/Decorators.ts
@@ -2,7 +2,7 @@
  * @ author: richen
  * @ copyright: Copyright (c) - <richenlin(at)gmail.com>
  * @ license: MIT
- * @ version: 2019-10-10 18:00:13
+ * @ version: 2019-10-11 21:08:36
  */
 // tslint:disable-next-line: no-import-side-effect
 import 'reflect-metadata';
@@ -12,20 +12,20 @@ import * as logger from "think_logger";
 import { saveModule, saveClassMetadata, savePropertyDataToClass, getIdentifier, getMethodNames } from "./Injectable";
 import { CONTROLLER_KEY, COMPONENT_KEY, TAGGED_PROP, TAGGED_CLS, TAGGED_ARGS, MIDDLEWARE_KEY, NAMED_TAG } from './Constants';
 
-export function Component(identifier?: any): ClassDecorator {
+export function Component(identifier?: string): ClassDecorator {
     return (target: any) => {
         saveModule(COMPONENT_KEY, target, identifier);
         saveClassMetadata(COMPONENT_KEY, TAGGED_CLS, identifier, target);
     };
 }
-export function Autowired(identifier?: any): PropertyDecorator {
+export function Autowired(identifier?: string): PropertyDecorator {
     return (target: any, propertyKey: string) => {
         identifier = identifier || helper.camelCase(propertyKey, { pascalCase: true });
         savePropertyDataToClass(TAGGED_PROP, identifier, target, propertyKey);
     };
 }
 
-export function Controller(path?: any): ClassDecorator {
+export function Controller(path?: string): ClassDecorator {
     return (target: any) => {
         saveModule(CONTROLLER_KEY, target);
         const identifier = getIdentifier(target);
@@ -33,13 +33,13 @@ export function Controller(path?: any): ClassDecorator {
         savePropertyDataToClass(NAMED_TAG, path, target, identifier);
     };
 }
-export function Middleware(identifier?: any): ClassDecorator {
+export function Middleware(identifier?: string): ClassDecorator {
     return (target: any) => {
         saveModule(MIDDLEWARE_KEY, target);
         saveClassMetadata(MIDDLEWARE_KEY, TAGGED_CLS, identifier, target);
     };
 }
-export function Service(identifier?: any): ClassDecorator {
+export function Service(identifier?: string): ClassDecorator {
     return (target: any) => {
         saveModule(COMPONENT_KEY, target, identifier);
         saveClassMetadata(COMPONENT_KEY, TAGGED_CLS, identifier, target);
