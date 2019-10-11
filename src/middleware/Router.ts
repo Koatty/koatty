d48c1d286778c74d4600a4b8e599b189962fdb01
richen <richenlin@gmail.com>
2019-10-11 09:40:18 +0800

    Success runing...

--- src/middleware/Router.ts
+++ src/middleware/Router.ts
@@ -2,7 +2,7 @@
  * @ author: richen
  * @ copyright: Copyright (c) - <richenlin(at)gmail.com>
  * @ license: MIT
- * @ version: 2019-10-09 16:08:20
+ * @ version: 2019-10-11 09:34:59
  */
 
 import KoaRouter from 'koa-router';
