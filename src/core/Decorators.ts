30857ddb68e4f532838022f90accefaf868ecebb
richen <richenlin@gmail.com>
2019-10-17 14:24:24 +0800

    Use typescript

--- src/core/Decorators.ts
+++ src/core/Decorators.ts
@@ -2,7 +2,7 @@
  * @ author: richen
  * @ copyright: Copyright (c) - <richenlin(at)gmail.com>
  * @ license: MIT
- * @ version: 2019-10-16 18:22:27
+ * @ version: 2019-10-17 14:22:24
  */
 // tslint:disable-next-line: no-import-side-effect
 import 'reflect-metadata';
@@ -74,12 +74,22 @@ export function Service(identifier?: string): ClassDecorator {
  *
  * @export
  * @param {string} [identifier]
+ * @param {CompomentType} [type]
  * @returns {PropertyDecorator}
  */
-export function Autowired(identifier?: string, type: CompomentType = 'SERVICE'): PropertyDecorator {
+export function Autowired(identifier?: string, type?: CompomentType): PropertyDecorator {
     return (target: any, propertyKey: string) => {
         const designType = Reflect.getMetadata('design:type', target, propertyKey);
         identifier = identifier || designType.name;
+        if (type === undefined) {
+            if (identifier.indexOf('Controller') > -1) {
+                type = 'CONTROLLER';
+            } else if (identifier.indexOf('Model') > -1) {
+                type = 'COMPONENT';
+            } else {
+                type = 'SERVICE';
+            }
+        }
         savePropertyDataToClass(TAGGED_PROP, `${type}:${identifier}`, target, propertyKey);
     };
 }
