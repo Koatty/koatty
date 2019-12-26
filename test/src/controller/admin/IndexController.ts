602275a638133aebe71d717cc67fc93a4be10639
richen <richenlin@gmail.com>
2019-12-26 11:48:06 +0800

    test case

--- test/src/controller/admin/IndexController.ts
+++ test/src/controller/admin/IndexController.ts
@@ -2,14 +2,14 @@
  * @ author: xxx
  * @ copyright: Copyright (c)
  * @ license: Apache License 2.0
- * @ version: 2019-12-13 11:10:16
+ * @ version: 2019-12-26 11:38:00
  */
 import { Controller, GetMaping, Autowired, RequestMapping, RequestMethod, PathVariable, PostMaping, RequestBody, Valid, Get } from "../../../../src/index";
 import { App } from '../../App';
 import { AdminController } from "../AdminController";
 import { TestService } from "../../service/TestService";
 
-@Controller("/admin")
+@Controller("/")
 export class IndexController extends AdminController {
     app: App;
     pageInfo: { 'appName': string; 'appVersion': string; 'appKeywords': string; 'appDescription': string; };
@@ -25,7 +25,7 @@ export class IndexController extends AdminController {
     @RequestMapping("/", RequestMethod.ALL)
     async default(@PathVariable("test") @Valid("notEmpty") test: string) {
         const info = await this.testService.test();
-        return this.ok(test, info);
+        return this.body(info);
     }
 
 }
--- test/src/service/TestService.ts
+++ test/src/service/TestService.ts
@@ -2,7 +2,7 @@
  * @ author: xxx
  * @ copyright: Copyright (c)
  * @ license: Apache License 2.0
- * @ version: 2019-12-02 14:49:55
+ * @ version: 2019-12-26 11:16:30
  */
 import { Service, Base, Autowired, Scheduled } from "../../../src/index";
 import { App } from '../App';
@@ -17,6 +17,7 @@ export class TestService extends CommonService {
     }
 
     async test() {
-        return Promise.reject("aa");
+        // return Promise.reject("aa");
+        return Promise.resolve("success");
     }
 }
