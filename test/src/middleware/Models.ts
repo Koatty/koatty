e3de6ef1cc3c6ddd93111ab444e56663ef98cd08
richen <richenlin@gmail.com>
2020-03-05 11:00:03 +0800

    Test case

--- test/src/middleware/Models.ts
+++ test/src/middleware/Models.ts
@@ -2,7 +2,7 @@
  * @ author: richen
  * @ copyright: Copyright (c) - <richenlin(at)gmail.com>
  * @ license: MIT
- * @ version: 2020-02-24 16:06:35
+ * @ version: 2020-03-05 10:35:01
  */
 import { Middleware, Helper } from "../../../src/index";
 
@@ -13,7 +13,8 @@ export class Models {
     run(options: any, app: any) {
         Helper.define(app, "mm1", 111);
         //应用启动执行一次
-        app.once('appReady', () => {
+        app.once('appReady', async () => {
+            console.log('Models Middleware');
             Helper.define(app, "mm2", 222);
         });
         return function (ctx: any, next: any) {
--- test/src/middleware/Zadtest.ts
+++ test/src/middleware/Zadtest.ts
@@ -2,7 +2,7 @@
  * @ author: richen
  * @ copyright: Copyright (c) - <richenlin(at)gmail.com>
  * @ license: MIT
- * @ version: 2020-02-24 16:06:42
+ * @ version: 2020-03-05 10:35:16
  */
 import { Middleware, Helper } from "../../../src/index";
 
@@ -11,6 +11,10 @@ const defaultOptions = {};
 @Middleware()
 export class Zadtest {
     run(options: any, app: any) {
+        //应用启动执行一次
+        app.once('appReady', async () => {
+            console.log('Zadtest Middleware');
+        });
         return function (ctx: any, next: any) {
             console.log('222222');
             return next();
