349b7c8ffa0fa04c7202d91bed17b3c82cc88d56
richen <richenlin@gmail.com>
2020-03-23 03:58:34 +0800

    Test case

--- test/src/model/TestDto.ts
+++ test/src/model/TestDto.ts
@@ -3,15 +3,15 @@ import { Length, Min, Max, IsCnName, Contains, IsNotEmpty, IsDefined, Component 
  * @ author: richen
  * @ copyright: Copyright (c) - <richenlin(at)gmail.com>
  * @ license: MIT
- * @ version: 2020-03-20 11:44:55
+ * @ version: 2020-03-23 03:19:07
  */
 @Component()
 export class TestDto {
     // @Length(10, 20, {
     //     message: "name必填或格式不正确"
     // })
-    // @IsNotEmpty({ message: "name不能为空" })
-    @IsDefined()
+    @IsNotEmpty({ message: "name不能为空" })
+    // @IsDefined()
     name: number;
 
     @Contains("hello")
--- test/src/service/TestService.ts
+++ test/src/service/TestService.ts
@@ -2,7 +2,7 @@
  * @ author: xxx
  * @ copyright: Copyright (c)
  * @ license: Apache License 2.0
- * @ version: 2020-03-19 14:12:48
+ * @ version: 2020-03-23 03:49:55
  */
 import { Service, Base, Autowired, Scheduled, Cacheable } from "../../../src/index";
 import { App } from '../App';
