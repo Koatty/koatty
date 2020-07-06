4ec067ba2e125eaab8f409e841a141b8b054cb09
richen <richenlin@gmail.com>
2020-07-06 19:46:17 +0800

    Validation method

--- src/core/Constants.ts
+++ src/core/Constants.ts
@@ -10,8 +10,6 @@ export const CONFIGUATION_SCAN = 'CONFIGUATION_SCAN';
 export const PRIORITY_KEY = 'PRIORITY_KEY';
 
 export const PARAM_KEY = 'PARAM_KEY';
-export const PARAM_RULE_KEY = 'PARAM_RULE_KEY';
-export const PARAM_CHECK_KEY = 'PARAM_CHECK_KEY';
 export const CONTROLLER_ROUTER = 'CONTROLLER_ROUTER';
 export const ROUTER_KEY = 'ROUTER_KEY';
 export const SCHEDULE_KEY = 'SCHEDULE_KEY';
