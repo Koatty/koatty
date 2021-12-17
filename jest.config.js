9858b0e8e217ee6959c16997dd5c8fa100334896
richen <richenlin@gmail.com>
2021-12-18 01:43:32 +0800

    🔧 build:

--- jest.config.js
+++ jest.config.js
@@ -25,8 +25,6 @@ module.exports = {
   // 将 `ts-jest` 的配置注入到运行时的全局变量中
   globals: {
     'ts-jest': {
-      // 是否使用 babel 配置来转译
-      babelConfig: true,
       // 编译 Typescript 所依赖的配置
       tsconfig: '<rootDir>/tsconfig.json',
       // 是否启用报告诊断，这里是不启用
---
+++ rollup.config.js
@@ -0,0 +1,63 @@
+/*
+ * @Description: 
+ * @Usage: 
+ * @Author: richen
+ * @Date: 2021-12-17 10:20:44
+ * @LastEditTime: 2021-12-18 00:47:46
+ */
+import json from "@rollup/plugin-json";
+import typescript from 'rollup-plugin-typescript2';
+// import babel from '@rollup/plugin-babel';
+
+export default [
+    {
+        input: './src/index.ts',
+        output: [{
+            format: 'cjs',
+            file: './dist/index.js',
+            banner: require('./scripts/copyright')
+        }],
+        plugins: [
+            // babel({
+            //     babelHelpers: "runtime",
+            //     configFile: './babel.config.js',
+            //     exclude: 'node_modules/**',
+            // }),
+            json(),
+            typescript({
+                tsconfigOverride: {
+                    compilerOptions: {
+                        declaration: false,
+                        declarationMap: false,
+                        module: "esnext"
+                    }
+                }
+            })
+        ]
+    },
+    {
+        input: './src/index.ts',
+        output: [{
+            format: 'es',
+            file: './dist/index.mjs',
+            banner: require('./scripts/copyright')
+        }],
+        plugins: [
+            // babel({
+            //     babelHelpers: "runtime",
+            //     configFile: './babel.config.js',
+            //     exclude: 'node_modules/**',
+            // }),
+            json(),
+            typescript({
+                tsconfigOverride: {
+                    compilerOptions: {
+                        declaration: false,
+                        declarationMap: false,
+                        module: "esnext"
+                    }
+                }
+            })
+        ]
+    }
+]
