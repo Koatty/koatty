3b4066cc580b98080ca9f853a03b762dc4beacb0
richen <richenlin@gmail.com>
2022-02-23 18:13:03 +0800

    ✨ feat: 增加jest单元测试支持

--- src/core/Bootstrap.ts
+++ src/core/Bootstrap.ts
@@ -5,15 +5,16 @@
  * @ version: 2020-07-06 11:22:58
  */
 import "reflect-metadata";
+import fs from "fs";
 import EventEmitter from "events";
-import { NewRouter } from "koatty_router";
-import { Koatty, KoattyRouterOptions } from 'koatty_core';
-import { BindProcessEvent, Serve } from "koatty_serve";
+import { Koatty } from 'koatty_core';
+import { NewRouter, RouterOptions } from "koatty_router";
 import { IOCContainer, TAGGED_CLS } from "koatty_container";
+import { BindProcessEvent, Serve, ListeningOptions } from "koatty_serve";
 import { Loader } from "./Loader";
 import { Helper } from "../util/Helper";
 import { Logger } from "../util/Logger";
-import { checkRuntime, KOATTY_VERSION } from "../util/Version";
+import { checkRuntime, checkUTRuntime, KOATTY_VERSION } from "../util/Check";
 import { APP_READY_HOOK, COMPONENT_SCAN, CONFIGURATION_SCAN, LOGO } from "./Constants";
 
 /**
@@ -21,14 +22,29 @@ import { APP_READY_HOOK, COMPONENT_SCAN, CONFIGURATION_SCAN, LOGO } from "./Cons
  *
  * @param {*} target
  * @param {Function} bootFunc
+ * @param {boolean} [isInitiative=false] Whether to actively execute app instantiation, 
+ * mainly for unittest scenarios, you need to actively obtain app instances
  * @returns {Promise<void>}
  */
-const ExecBootstrap = async function (target: any, bootFunc: Function): Promise<void> {
+const executeBootstrap = async function (target: any, bootFunc: Function, isInitiative = false): Promise<void> {
     // checked runtime
     checkRuntime();
+    // unittest running environment
+    const isUTRuntime = checkUTRuntime();
+    if (!isInitiative && isUTRuntime) {
+        return;
+    }
+
     const app = Reflect.construct(target, []);
+    // unittest does not print startup logs
+    if (isUTRuntime) {
+        app.silent = true;
+        Logger.setLogConsole(false);
+        Logger.setLogFile(false);
+    }
+
     try {
-        console.log(LOGO);
+        !app.silent && console.log(LOGO);
         if (!(app instanceof Koatty)) {
             throw new Error(`class ${target.name} does not inherit from Koatty`);
         }
@@ -97,15 +113,14 @@ const ExecBootstrap = async function (target: any, bootFunc: Function): Promise<
         Logger.Log('think', '', 'Load Routers ...');
         KoattyRouter.LoadRouter(app.getMetaData("_controllers"));
 
-        // Emit app started event
-        Logger.Log('think', '', 'Emit App Start ...');
-        // app.emit("appStart");
-        const server = newServe(app)
-        await asyncEvent(app, 'appStart');
-
         Logger.Log('think', '', '====================================');
         // Start server
-        app.listen(server);
+        const server = newServe(app)
+        app.listen(server, listenCallback(app, server.options));
+        // app.emit("appStart");
+        // Emit app started event
+        Logger.Log('think', '', 'Emit App Start ...');
+        asyncEvent(app, 'appStart');
 
         // binding event "appStop"
         BindProcessEvent(app, 'appStop');
@@ -124,12 +139,31 @@ const ExecBootstrap = async function (target: any, bootFunc: Function): Promise<
  */
 const newRouter = function (app: Koatty) {
     const protocol = app.config("protocol") || "http";
-    const options: KoattyRouterOptions = app.config(undefined, 'router') ?? {};
+    const options: RouterOptions = app.config(undefined, 'router') ?? {};
     const router = NewRouter(app, options, protocol);
 
     Helper.define(app, "router", router);
     return router;
 }
+
+/**
+ * Listening callback function
+ *
+ * @param {Koatty} app
+ * @param {ListeningOptions} options
+ * @returns {*} 
+ */
+const listenCallback = (app: Koatty, options: ListeningOptions) => {
+    return function () {
+        Logger.Log("think", "", `Nodejs Version: ${process.version}`);
+        Logger.Log("think", "", `Koatty Version: v${app.version}`);
+        Logger.Log("think", "", `App Environment: ${app.env}`);
+        Logger.Log("think", "", `Server running at ${options.protocol === "http2" ? "https" : options.protocol}://${options.hostname || '127.0.0.1'}:${options.port}/`);
+        Logger.Log("think", "", "====================================");
+        // tslint:disable-next-line: no-unused-expression
+        app.appDebug && Logger.Warn(`Running in debug mode.`);
+    };
+};
 
 /**
  * create serve
@@ -139,13 +173,39 @@ const newRouter = function (app: Koatty) {
  */
 const newServe = function (app: Koatty) {
     const protocol = app.config("protocol") || "http";
-    const server = Serve(app, protocol);
+    const port = process.env.PORT || process.env.APP_PORT ||
+        app.config('app_port') || 3000;
+    const hostname = process.env.IP ||
+        process.env.HOSTNAME?.replace(/-/g, '.') || app.config('app_host') || '127.0.0.1';
+    const options: ListeningOptions = {
+        hostname,
+        port,
+        protocol,
+        ext: {
+            key: "",
+            cert: "",
+            protoFile: "",
+        },
+    };
+    const pm = new Set(["https", "http2", "wss"])
+    if (pm.has(options.protocol)) {
+        const keyFile = app.config("key_file") ?? "";
+        const crtFile = app.config("crt_file") ?? "";
+        options.ext.key = fs.readFileSync(keyFile).toString();
+        options.ext.cert = fs.readFileSync(crtFile).toString();
+    }
+    if (options.protocol === "https" || options.protocol === "http2") {
+        options.port = options.port == 80 ? 443 : options.port;
+    }
+    if (options.protocol === "grpc") {
+        const proto = app.config("protoFile", "router");
+        options.ext.protoFile = proto;
+    }
+    const server = Serve(app, options);
 
     Helper.define(app, "server", server);
     return server;
 }
-
-
 
 /**
  * Execute event as async
@@ -176,7 +236,24 @@ export function Bootstrap(bootFunc?: Function): ClassDecorator {
         if (!(target.prototype instanceof Koatty)) {
             throw new Error(`class does not inherit from Koatty`);
         }
-        ExecBootstrap(target, bootFunc);
+        executeBootstrap(target, bootFunc);
+    };
+}
+
+
+/**
+ * Actively perform dependency injection
+ * Parse the decorator, return the instantiated app. 
+ * @export  ExecBootStrap
+ * @param {Function} [bootFunc] callback function
+ * @returns
+ */
+export function ExecBootStrap(bootFunc?: Function) {
+    return async (target: any) => {
+        if (!(target.prototype instanceof Koatty)) {
+            throw new Error(`class ${target.name} does not inherit from TKoatty`);
+        }
+        return await executeBootstrap(target, bootFunc, true);
     };
 }
 
