5f46b1a31eac56da49dd3abb333860bad6351969
richen <richenlin@gmail.com>
2023-09-30 09:37:43 +0800

    refactor: demo

--- demo/src/exception/BussinessException.ts
+++ demo/src/exception/BussinessException.ts
@@ -3,26 +3,34 @@
  * @Usage: 
  * @Author: richen
  * @Date: 2022-02-14 11:26:20
- * @LastEditTime: 2023-07-27 23:14:28
+ * @LastEditTime: 2023-09-25 19:14:04
  */
 
 import { KoattyContext } from "koatty_core";
-import { Exception, ExceptionHandler } from "koatty_trace";
+import { Exception, ExceptionHandler } from "koatty_exception";
 
-// @ExceptionHandler()
+@ExceptionHandler()
 export class BussinessException extends Exception {
   async handler(ctx: KoattyContext): Promise<any> {
     ctx.status = this.status;
     ctx.type = "application/json";
-    const body: any = ctx.body ? JSON.stringify(ctx.body) : (ctx.body || null);
+    const body: any = JSON.stringify(ctx.body || null);
     switch (ctx.protocol) {
       case "ws":
       case "wss":
-        return ctx?.websocket?.send(body, () => ctx?.websocket?.emit('finish'));
+        if (ctx.websocket) {
+          ctx.websocket.send(body);
+          ctx.websocket.emit('finish');
+        }
+        break;
       case "grpc":
-        return ctx.rpc ? ctx.rpc.callback!(null, body) : null;
+        if (ctx.rpc && ctx.rpc.callback) {
+          ctx.rpc.callback(null, body);
+        }
+        break;
       default:
-        return ctx.res.end(`{"code": ${this.code}, "message": "${this.message || ctx.message}", "data": ${body}}`);
+        ctx.res.end(`{"code": ${this.code}, "message": "${this.message || ctx.message}", "data": ${body}}`);
+        break;
     }
   }
 }
