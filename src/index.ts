b2eed18017838e9e91394f91cd60ea9790c59920
richen <richenlin@gmail.com>
2023-12-20 20:30:04 +0800

    fix: remove export

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  * @Usage: 
  * @Author: richen
  * @Date: 2023-12-09 21:56:32
- * @LastEditTime: 2023-12-09 23:02:40
+ * @LastEditTime: 2023-12-20 20:28:04
  * @License: BSD (3-Clause)
  * @Copyright (c): <richenlin(at)gmail.com>
  */
@@ -16,7 +16,6 @@ export * from "koatty_core";
 export * from "koatty_container";
 export * from "koatty_exception";
 export * from "koatty_serve";
-export * from "koatty_trace";
 export { Value, Config } from "koatty_config";
 export { Helper } from "./util/Helper";
 export { Logger } from "./util/Logger";
